1ee7ac1003e7008de8da0b3dc5460d50285e7d16
qi0523 <1426042225@qq.comclear>
2023-12-26 20:15:35 +0800

    first

---
+++ controller_start.sh
@@ -0,0 +1,387 @@
+#!/bin/bash
+
+set -x
+
+USER=Zhihao
+USER_GROUP=containernetwork
+MASTER_PORT=3000
+INVOKER_PORT=3001
+INSTALL_DIR=/home/cloudlab-openwhisk
+HOST_ETH0_IP=$(ifconfig eth1 | awk 'NR==2{print $2}')
+HOST_NAME=$(hostname | awk 'BEGIN{FS="."} {print $1}')
+BASE_IP="12.12.10."
+
+# change hostname
+sudo hostnamectl set-hostname $HOST_NAME
+sudo sed -i "4a 127.0.0.1 $HOST_NAME" /etc/hosts
+
+#role: control-plane
+
+## modify containerd, TODO:
+sudo apt update
+sudo apt install -y apparmor apparmor-utils
+
+## cni plugins TODO:
+sudo chown -R $USER:$USER_GROUP $INSTALL_DIR
+pushd $INSTALL_DIR/install
+# wget https://github.com/containernetworking/plugins/releases/download/v1.1.1/cni-plugins-linux-amd64-v1.1.1.tgz
+sudo tar Cxzvf /opt/cni/bin cni-plugins-linux-amd64-v1.1.1.tgz
+popd
+
+# sudo sed -i 's/GRUB_CMDLINE_LINUX="/GRUB_CMDLINE_LINUX="cgroup_enable=memory swapaccount=1,/' /etc/default/grub
+# sudo update-grub
+
+## modify containerd
+configure_docker_storage() {
+    printf "%s: %s\n" "$(date +"%T.%N")" "Configuring containerd storage"
+    sudo mkdir -p /mydata/var/lib/containerd
+    sudo sed -i 's#root = "/var/lib/containerd"#root = "/mydata/var/lib/containerd"#g' /etc/containerd/config.toml
+    sudo systemctl restart containerd || (echo "ERROR: containerd installation failed, exiting." && exit -1)
+    printf "%s: %s\n" "$(date +"%T.%N")" "Configured containerd storage to use mountpoint"
+}
+
+## memory.memsw
+disable_swap() {
+    # Turn swap off and comment out swap line in /etc/fstab
+    sudo swapoff -a
+    if [ $? -eq 0 ]; then   
+        printf "%s: %s\n" "$(date +"%T.%N")" "Turned off swap"
+    else
+        echo "***Error: Failed to turn off swap, which is necessary for Kubernetes"
+        exit -1
+    fi
+    sudo sed -i 's/UUID=.*swap/# &/' /etc/fstab
+}
+
+#invoker ip array
+invoker_ips=()
+
+wait_invokers_ip(){
+    # $1 == invoker nums
+    NUM_REGISTERED=0
+    NUM_UNREGISTERED=$(($1-NUM_REGISTERED))
+    while [ "$NUM_UNREGISTERED" -ne 0 ]
+    do
+        sleep 0.5
+        if [ -z "$nc_PID" ]
+        then
+            printf "%s: %s\n" "$(date +"%T.%N")" "Restarting listener via netcat..."
+            coproc nc { nc -l $HOST_ETH0_IP $MASTER_PORT; }
+        fi
+        read -r -u${nc[0]} INVOKER_IP
+        printf "%s: %s\n" "$(date +"%T.%N")" "read invoker ip: $INVOKER_IP"
+        invoker_ips[$NUM_REGISTERED]=$INVOKER_IP
+        NUM_REGISTERED=$(($NUM_REGISTERED+1))
+        NUM_UNREGISTERED=$(($1-NUM_REGISTERED))
+    done
+}
+
+wait_ips(){
+    while [ ! -f "/home/cloudlab-openwhisk/ok.txt" ]
+    do
+        sleep 60
+    done
+
+    ips=$(cat /home/cloudlab-openwhisk/ips.txt)
+    invoker_ips=($ips)
+}
+
+setup_primary() {
+
+    # Download and install helm
+    pushd $INSTALL_DIR/install
+    curl -fsSL -o get_helm.sh https://raw.githubusercontent.com/helm/helm/master/scripts/get-helm-3
+    chmod 744 get_helm.sh
+    sudo ./get_helm.sh
+    popd
+
+    # initialize k8 primary node
+    printf "%s: %s\n" "$(date +"%T.%N")" "Starting Kubernetes... (this can take several minutes)... "
+    sudo kubeadm init --apiserver-advertise-address=$HOST_ETH0_IP --pod-network-cidr=10.240.0.0/12 > $INSTALL_DIR/k8s_install.log 2>&1
+    if [ $? -eq 0 ]; then
+        printf "%s: %s\n" "$(date +"%T.%N")" "Done! Output in $INSTALL_DIR/k8s_install.log"
+    else
+        echo ""
+        echo "***Error: Error when running kubeadm init command. Check log found in $INSTALL_DIR/k8s_install.log."
+        exit 1
+    fi
+
+    # Set up kubectl for Zhihao users TODO: completed
+    sudo mkdir /users/$USER/.kube
+    sudo cp /etc/kubernetes/admin.conf /users/$USER/.kube/config
+    sudo chown -R $USER:$USER_GROUP /users/$USER/.kube
+	printf "%s: %s\n" "$(date +"%T.%N")" "set /users/$USER/.kube to $USER:$USER_GROUP!"
+	ls -lah /users/$USER/.kube
+    printf "%s: %s\n" "$(date +"%T.%N")" "Done!"
+
+    ### TODO: remove taint master, completed
+    kubectl taint nodes --all node-role.kubernetes.io/master-
+    kubectl taint nodes --all node-role.kubernetes.io/control-plane-
+}
+
+apply_flannel() {
+    # flannel
+    pushd $INSTALL_DIR/install
+    wget https://raw.githubusercontent.com/coreos/flannel/master/Documentation/kube-flannel.yml
+    sed -i 's/10.244.0.0\/16/10.240.0.0\/12/g' kube-flannel.yml
+    kubectl apply -f kube-flannel.yml >> $INSTALL_DIR/flannel_install.log 2>&1
+    popd
+    # https://projectcalico.docs.tigera.io/getting-started/kubernetes/helm
+    printf "%s: %s\n" "$(date +"%T.%N")" "Loaded flannel pods"
+    if [ $? -ne 0 ]; then
+       echo "***Error: Error when installing flannel. Log appended to $INSTALL_DIR/flannel_install.log"
+       exit 1
+    fi
+    printf "%s: %s\n" "$(date +"%T.%N")" "Applied flannel networking from "
+
+    # wait for flannel pods to be in ready state
+    printf "%s: %s\n" "$(date +"%T.%N")" "Waiting for flannel pods to have status of 'Running': "
+    sleep 10
+    NUM_PODS=$(kubectl get pods -A | grep flannel | wc -l)
+    while [ "$NUM_PODS" -eq 0 ]
+    do
+        sleep 5
+        printf "."
+        NUM_PODS=$(kubectl get pods -A | grep flannel | wc -l)
+    done
+    NUM_RUNNING=$(kubectl get pods -A | grep flannel | grep " Running" | wc -l)
+    NUM_RUNNING=$((NUM_PODS-NUM_RUNNING))
+    while [ "$NUM_RUNNING" -ne 0 ]
+    do
+        sleep 5
+        printf "."
+        NUM_RUNNING=$(kubectl get pods -A | grep flannel | grep " Running" | wc -l)
+        NUM_RUNNING=$((NUM_PODS-NUM_RUNNING))
+    done
+    printf "%s: %s\n" "$(date +"%T.%N")" "flannel running!"
+}
+
+add_cluster_nodes() { ## $1 == 1
+   # awk -v line=$(awk '{if($1=="kubeadm")print NR}' k8s.log) '{if(NR>=line && NR<line+2){print $0}}' k8s.log
+    REMOTE_CMD=$(awk -v line=$(awk '{if($1=="kubeadm")print NR}' $INSTALL_DIR/k8s_install.log) '{if(NR>=line && NR<line+2){print $0}}' $INSTALL_DIR/k8s_install.log)
+    printf "%s: %s\n" "$(date +"%T.%N")" "Remote command is: $REMOTE_CMD"
+
+    NUM_REGISTERED=$(kubectl get nodes | wc -l)
+    NUM_REGISTERED=$(($1-NUM_REGISTERED+1))
+    counter=0
+    while [ "$NUM_REGISTERED" -ne 0 ]
+    do 
+	sleep 2
+        printf "%s: %s\n" "$(date +"%T.%N")" "Registering nodes, attempt #$counter, registered=$NUM_REGISTERED"
+        for (( i=1; i<=$1; i++ ))
+        do
+            SECONDARY_IP=$BASE_IP$i
+            echo $SECONDARY_IP
+            exec 3<>/dev/tcp/$SECONDARY_IP/$INVOKER_PORT
+            echo $REMOTE_CMD 1>&3
+            exec 3<&-
+        done
+	counter=$((counter+1))
+        NUM_REGISTERED=$(kubectl get nodes | wc -l)
+        NUM_REGISTERED=$(($1-NUM_REGISTERED+1)) 
+    done
+
+    printf "%s: %s\n" "$(date +"%T.%N")" "Waiting for all nodes to have status of 'Ready': "
+    NUM_READY=$(kubectl get nodes | grep " Ready" | wc -l)
+    NUM_READY=$(($1-NUM_READY))
+    while [ "$NUM_READY" -ne 0 ]
+    do
+        sleep 10
+        printf "."
+        NUM_READY=$(kubectl get nodes | grep " Ready" | wc -l)
+        NUM_READY=$(($1-NUM_READY))
+    done
+    printf "%s: %s\n" "$(date +"%T.%N")" "Done!" 
+}
+
+add_cluster_nodes_scale() { ## $1 == 1
+
+    # awk -v line=$(awk '{if($1=="kubeadm")print NR}' k8s.log) '{if(NR>=line && NR<line+2){print $0}}' k8s.log
+    REMOTE_CMD=$(awk -v line=$(awk '{if($1=="kubeadm")print NR}' $INSTALL_DIR/k8s_install.log) '{if(NR>=line && NR<line+2){print $0}}' $INSTALL_DIR/k8s_install.log)
+    printf "%s: %s\n" "$(date +"%T.%N")" "Remote command is: $REMOTE_CMD"
+
+    for (( i=0; i<$1; i++ ))
+    do
+        INVOKER_IP=${invoker_ips[$i]}
+        echo $INVOKER_IP
+        exec 3<>/dev/tcp/$INVOKER_IP/$INVOKER_PORT
+        while [ "$?" -ne 0 ]
+        do
+            sleep 2
+            exec 3<>/dev/tcp/$1/$MASTER_PORT
+        done
+        echo $REMOTE_CMD 1>&3
+        exec 3<&-
+    done
+
+    printf "%s: %s\n" "$(date +"%T.%N")" "Waiting for all nodes to have status of 'Ready': "
+    NUM_READY=$(kubectl get nodes | grep " Ready" | wc -l)
+    NUM_READY=$(($1-NUM_READY+1))
+    while [ "$NUM_READY" -ne 0 ]
+    do
+        sleep 10
+        printf "."
+        NUM_READY=$(kubectl get nodes | grep " Ready" | wc -l)
+        NUM_READY=$(($1-NUM_READY+1))
+    done
+    printf "%s: %s\n" "$(date +"%T.%N")" "Done!"
+}
+
+prepare_for_openwhisk() {
+    # Args: 1 = IP, 2 = num nodes, 3 = num invokers, 4 = invoker engine
+
+    # TODO: nfs-server
+    sudo apt-get update
+    sudo apt install nfs-kernel-server -y
+    NFS_DIR=/proj/containernetwork-PG0/data/nfs
+    sudo mkdir -p $NFS_DIR
+    sudo chmod 777 $NFS_DIR
+
+    echo "$NFS_DIR *(fsid=0,rw,sync,no_root_squash)" | sudo tee /etc/exports
+    sudo systemctl restart nfs-server
+    if [ "$?" -ne 0 ]; then
+        echo "nfs server failed..."
+        exit 1
+    fi
+
+    # nfs-subdir-external-provisioner
+    helm repo add nfs-subdir-external-provisioner https://kubernetes-sigs.github.io/nfs-subdir-external-provisioner/
+    helm install nfs-provisioner nfs-subdir-external-provisioner/nfs-subdir-external-provisioner \
+        --set nfs.server=$HOST_ETH0_IP \
+        --set nfs.path=$NFS_DIR \
+        --set storageClass.defaultClass=true
+
+    if [ "$?" -ne 0 ]; then
+        echo "nfs provisioner failed..."
+        exit 1
+    fi
+
+    # k8s sc
+    nfs_client_running_num=$(kubectl get pods -A | grep nfs-provisioner | grep Running | wc -l)
+    while [ "$nfs_client_running_num" -eq 0 ]
+    do
+        sleep 3
+        echo "wait for nfs_provisioner_running...."
+        nfs_client_running_num=$(kubectl get pods -A | grep nfs-provisioner | grep Running | wc -l)
+    done
+
+    #label nodes=core 
+    CONTROLLER_NODE=$(kubectl get nodes | grep master | awk '{print $1}')
+    kubectl label nodes ${CONTROLLER_NODE} openwhisk-role=core
+    # label nodes=invoker
+    INVOKER_NODES=$(kubectl get nodes | grep ow | awk '{print $1}')
+    while IFS= read -r line; do
+        kubectl label nodes ${line} openwhisk-role=invoker
+        if [ $? -ne 0 ]; then
+            echo "***Error: Failed to set openwhisk role to invoker on ${line:5}."
+            exit -1
+        fi
+        printf "%s: %s\n" "$(date +"%T.%N")" "Labelled ${line} as openwhisk invoker node"
+    done <<< "$INVOKER_NODES"
+    printf "%s: %s\n" "$(date +"%T.%N")" "Finished labelling nodes."
+
+    # git clone qi0523/openwhisk-deploy-bue
+    git clone https://github.com/qi0523/openwhisk-deploy-kube $INSTALL_DIR/openwhisk-deploy-kube
+
+    pushd $INSTALL_DIR/openwhisk-deploy-kube
+    sed -i "s/REPLACE_ME_WITH_IP/$HOST_ETH0_IP/g" mycluster.yaml
+    popd
+    kubectl create namespace openwhisk
+    if [ $? -ne 0 ]; then
+        echo "***Error: Failed to create openwhisk namespace"
+        exit 1
+    fi
+    printf "%s: %s\n" "$(date +"%T.%N")" "Created openwhisk namespace in Kubernetes."
+
+    pushd $INSTALL_DIR/install
+    # Download and install the OpenWhisk CLI
+    wget https://github.com/apache/openwhisk-cli/releases/download/latest/OpenWhisk_CLI-latest-linux-386.tgz
+    sudo tar -xzvf OpenWhisk_CLI-latest-linux-386.tgz -C /usr/local/bin wsk
+
+    # Set up wsk properties for all users
+        echo -e "
+	APIHOST=$HOST_ETH0_IP:31001
+	AUTH=23bc46b1-71f6-4ed5-8c54-816aa4f8c502:123zO3xZCLrMN6v2BKK1dXYFpXlPkccOFqm12CdAsMgRU4VrNZ9lyGVCGuMDGIwP
+	" | sudo tee /users/$USER/.wskprops
+	sudo chown $USER:$USER_GROUP /users/$USER/.wskprops
+    popd
+}
+
+
+deploy_openwhisk() {
+    # Takes cluster IP as argument to set up wskprops files.
+
+    # Deploy openwhisk via helm
+    printf "%s: %s\n" "$(date +"%T.%N")" "About to deploy OpenWhisk via Helm... "
+    pushd $INSTALL_DIR/openwhisk-deploy-kube
+    helm install owdev ./helm/openwhisk -n openwhisk -f mycluster.yaml > $INSTALL_DIR/ow_install.log 2>&1 
+    if [ $? -eq 0 ]; then
+        printf "%s: %s\n" "$(date +"%T.%N")" "Ran helm command to deploy OpenWhisk"
+    else
+        echo ""
+        echo "***Error: Helm install error. Please check $INSTALL_DIR/ow_install.log."
+        exit 1
+    fi
+
+    # Monitor pods until openwhisk is fully deployed
+    kubectl get pods -n openwhisk
+    printf "%s: %s\n" "$(date +"%T.%N")" "Waiting for OpenWhisk to complete deploying (this can take several minutes): "
+    DEPLOY_COMPLETE=$(kubectl get pods -n openwhisk | grep owdev-install-packages | grep Completed | wc -l)
+    while [ "$DEPLOY_COMPLETE" -ne 1 ]
+    do
+        sleep 10
+        DEPLOY_COMPLETE=$(kubectl get pods -n openwhisk | grep owdev-install-packages | grep Completed | wc -l)
+    done
+    printf "%s: %s\n" "$(date +"%T.%N")" "OpenWhisk deployed!"
+    popd
+}
+
+# Start by recording the arguments
+printf "%s: args=(" "$(date +"%T.%N")"
+for var in "$@"
+do
+    printf "'%s' " "$var"
+done
+printf ")\n"
+
+# Kubernetes does not support swap, so we must disable it
+disable_swap
+
+# Use mountpoint (if it exists) to set up additional docker image storage
+if test -d "/mydata"; then
+    configure_docker_storage
+fi
+
+# Use second argument (node IP) to replace filler in kubeadm configuration
+sudo sed -i "s/REPLACE_ME_WITH_IP/$HOST_ETH0_IP/g" /etc/systemd/system/kubelet.service.d/10-kubeadm.conf
+
+#coproc nc { nc -l $HOST_ETH0_IP $MASTER_PORT; }
+
+# python $INSTALL_DIR/install/server.py $1 $HOST_ETH0_IP &
+
+# wait_ips $1
+
+setup_primary $HOST_ETH0_IP
+
+# Apply flannel networking
+apply_flannel
+
+# Coordinate master to add nodes to the kubernetes cluster
+# Argument is number of nodes
+add_cluster_nodes $1
+# add_cluster_nodes_scale $1
+
+# Exit early if we don't need to deploy OpenWhisk
+# if [ "$2" = "false" ]; then
+#     printf "%s: %s\n" "$(date +"%T.%N")" "Don't need to deploy Openwhisk!"
+#     exit 0
+# fi
+
+# Prepare cluster to deploy OpenWhisk: takes IP, num nodes, invoker num, and invoker engine
+prepare_for_openwhisk
+
+# Deploy OpenWhisk via Helm
+# Takes cluster IP
+#deploy_openwhisk $HOST_ETH0_IP
+
+printf "%s: %s\n" "$(date +"%T.%N")" "Profile setup completed!"
---
+++ profile.py
@@ -0,0 +1,180 @@
+"""Variable number of VMs.
+
+Instructions:
+Log into your node, use `sudo` to poke around.
+"""
+
+# A comment.    
+
+import geni.portal as portal
+import geni.rspec.pg as pg
+import geni.rspec.pg as rspec
+import geni.rspec.emulab
+
+#
+# This is a typical list of images.
+#
+image = "urn:publicid:IDN+cloudlab.umass.edu+image+containernetwork-PG0:k8s-reg-dlcl"
+
+pc = portal.Context()
+
+request = pc.makeRequestRSpec()
+
+pc.defineParameter("controller_cores",
+                   "Controller cpu cores",
+                   portal.ParameterType.INTEGER,
+                   12,
+                   longDescription="Controller cpu cores.")
+
+pc.defineParameter("controller_memory",
+                   "Controller memory",
+                   portal.ParameterType.INTEGER,
+                   65536,
+                   longDescription="Controller memory.")
+
+pc.defineParameter("hardware_type",
+                   "Optional physical node type (utah:xl170 |d6515|c6525-25g|c6525-100g  clem: r7525|r650|r6525)",
+                   portal.ParameterType.STRING, "rs620")
+
+pc.defineParameter("tempFileSystemSize", 
+                   "Temporary Filesystem Size",
+                   portal.ParameterType.INTEGER, 
+                   50,
+                   advanced=True,
+                   longDescription="The size in GB of a temporary file system to mount on each of your " +
+                   "nodes. Temporary means that they are deleted when your experiment is terminated. " +
+                   "The images provided by the system have small root partitions, so use this option " +
+                   "if you expect you will need more space to build your software packages or store " +
+                   "temporary files. 0 GB indicates maximum size.")
+
+pc.defineParameter("N", "Number of virtual nodes (containers)",
+                   portal.ParameterType.INTEGER, 4)
+pc.defineParameter("X", "Number of containers per physical node",
+                   portal.ParameterType.INTEGER, 16)
+
+pc.defineParameter("masterIP", 
+                   "Master ip address",
+                   portal.ParameterType.STRING, 
+                   "172.17.1.1")
+
+pc.defineParameter("disk",
+                   "Invoker gc disk",
+                   portal.ParameterType.INTEGER,
+                   1024,
+                   longDescription="Invoker bandwidth.")
+
+pc.defineParameter("bandwidth",
+                   "Invoker bandwidth",
+                   portal.ParameterType.INTEGER,
+                   500000,
+                   longDescription="Invoker bandwidth.")
+
+pc.defineParameter("registryIP", 
+                   "Registry ip address",
+                   portal.ParameterType.STRING, 
+                   "172.17.1.1")
+
+pc.defineParameter("lan",  "Put all nodes in a LAN",
+                   portal.ParameterType.BOOLEAN, True)
+pc.defineParameter("shared",  "Try to use shared nodes",
+                   portal.ParameterType.BOOLEAN, False)
+# pc.defineParameter("nobw",  "Disable BW and Shaping",
+#                    portal.ParameterType.BOOLEAN, False)
+pc.defineParameter("publicips",  "Request Public IPs",
+                   portal.ParameterType.BOOLEAN, True)
+pc.defineParameter("phystype",  "Optional physical node type",
+                   portal.ParameterType.NODETYPE, "",
+                   longDescription="d710-vm, etc. Be sure to pick the correct cluster on the next step")
+pc.defineParameter("physnode",  "Optional physical node",
+                   portal.ParameterType.STRING, "",
+                   longDescription="pc606, etc. Be sure to pick the correct cluster on the next step")
+                   
+#pc.defineParameter("phystype2",  "Filtered physical node type",
+#                   portal.ParameterType.NODETYPE, "",
+#                   longDescription="{'arch'  : 'x86_64', 'cores' : '>2', 'mem_size': '>=16384', 'disksize': '>=400'}",
+#                   inputConstraints={'arch'  : 'x86_64', 'cores' : '>2', 'mem_size': '>=16384', 'disksize': ">=400"})
+
+#pc.defineParameter("pubkey",  "public key",
+#                   portal.ParameterType.PUBKEY, "")
+params = pc.bindParameters()
+
+#
+# This option sets the maximum number of VMs that will be packed onto any single
+# physical node (subject to other constraints such as memory usage). For example,
+# if you set this number to 5 and ask for 10 VMs, the resource mapper will place no
+# more then 5 VMs on each physical node (that the mapper picks). The default number
+# if you do not set this option, is typically around 10 VMs (again, subject to
+# other constraints).
+#
+request.setCollocateFactor(params.X)
+#
+# But the mapper is a very fickle beast, and sometimes it will decide to put
+# less then the maximum number on each physical node, and sometimes it will,
+# but you want things spread out more evenly. This option can be set to either
+# "pack" or "balance" to change how the mapper's brain chemistry. But like
+# any antidepressant, every day is a new day. 
+#
+#request.setPackingStrategy("pack")
+lan = request.LAN()
+
+if (params.lan):
+    lan.vlan_tagging = True
+    # if params.nobw:
+    #     lan.trivial_ok = False
+    #     lan.setNoBandwidthShaping()
+    # else:
+    lan.trivial_ok = True
+    lan.bandwidth = params.bandwidth 
+    lan.setForceShaping() 
+        # pass
+    pass
+
+node1 = request.XenVM("master")
+
+node1.exclusive = True
+  
+node1.cores = params.controller_cores
+  # Ask for 2GB of ram
+node1.ram = params.controller_memory
+
+node1.disk_image = image
+
+node1.hardware_type = params.hardware_type
+
+# Add extra storage space
+if (params.tempFileSystemSize > 0):
+    bs1 = node1.Blockstore("master-bs", "/mydata")
+    bs1.size = str(params.tempFileSystemSize) + "GB"
+    bs1.placement = "any"
+
+iface1 = node1.addInterface("eth1")
+iface1.addAddress(pg.IPv4Address("12.12.1.1", "255.255.0.0"))
+lan.addInterface(iface1)
+node1.addService(rspec.Execute(shell="sh", command="bash /local/repository/controller_start.sh {} &".format(params.N)))
+
+for i in range(1, params.N+1):
+  node = request.XenVM("ow%d" % i)
+  node.ram = 8192
+  node.cores = 2
+  if params.shared:
+      node.exclusive = False
+  else:
+      node.exclusive = True
+      pass
+#   if params.publicips:
+#       node.routable_control_ip = True
+  node.disk_image = image 
+  node.disk = 0
+  if params.physnode != "":
+      node.component_id = params.physnode
+  elif params.phystype != "":
+      node.xen_ptype = params.phystype + "-vm"
+      pass
+    
+  if params.lan:
+    iface = node.addInterface("eth1")
+    iface.addAddress(pg.IPv4Address("12.12.10."+str(i), "255.255.0.0"))
+    lan.addInterface(iface)
+  node.addService(rspec.Execute(shell="sh", command="bash /local/repository/invoker_start.sh 12.12.1.1 {} {} {} &".format(params.bandwidth, params.registryIP, params.disk * 1024 * 1024)))
+
+pc.printRequestRSpec(request)
